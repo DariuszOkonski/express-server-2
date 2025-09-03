f265a108500cdb4a175ced4a3e6c006da45426f0
Dariusz Okoński <darek200180@gmail.com>
2025-09-03 18:57:15 +0200

    add fake database

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+
+// fake database
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  {
+    id: 2,
+    author: 'Amanda Doe',
+    text: 'They really know how to make you happy.',
+  },
+  { id: 3, author: 'Johnny Bravo', text: 'Hello Johnny Bravo' },
+];
 
 // Routes
 app.get('/testimonials', (req, res) => {

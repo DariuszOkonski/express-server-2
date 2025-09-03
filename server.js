03437ff36cc1f682244515fc99793615e2edbd6f
Dariusz Okoński <darek200180@gmail.com>
2025-09-03 18:54:23 +0200

    add middleware

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/testimonials', (req, res) => {
+  res.send('get testimonials');
+});
+
+app.get('/testimonials/:id', (req, res) => {
+  res.send('get testimonials/id');
+});
+
+app.get('/testimonials/random', (req, res) => {
+  res.send('get testimonials/random');
+});
+
+app.post('/testimonials', (req, res) => {
+  res.send('post testimonials');
+});
+
+app.put('/testimonials/:id', (req, res) => {
+  res.send('put testimonials/id');
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+  res.send('delete testimonials/id');
 });
 
 // Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

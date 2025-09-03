d0f271cdccf2317a6f9aea1f04e761a3d4abb8e4
Dariusz Okoński <darek200180@gmail.com>
2025-09-03 19:22:33 +0200

    working on put request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 
 // Middleware
 app.use(express.json());
@@ -8,33 +9,79 @@ app.use(express.urlencoded({ extended: false }));
 
 // fake database
 const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
   {
-    id: 2,
+    id: '43aaa7b2-6f32-416b-bef3-1d44fa6f781f',
+    author: 'John Doe',
+    text: 'This company is worth every coin!',
+  },
+  {
+    id: '38d7f8da-8fcb-484a-86c8-a0bc6847fd8b',
     author: 'Amanda Doe',
     text: 'They really know how to make you happy.',
   },
-  { id: 3, author: 'Johnny Bravo', text: 'Hello Johnny Bravo' },
+  {
+    id: '331b4ce8-c998-492d-8c70-04a70ad2e629',
+    author: 'Johnny Bravo',
+    text: 'Hello Johnny Bravo',
+  },
 ];
 
 // Routes
 app.get('/testimonials', (req, res) => {
-  res.send('get testimonials');
-});
-
-app.get('/testimonials/:id', (req, res) => {
-  res.send('get testimonials/id');
+  res.json({ data: db });
 });
 
 app.get('/testimonials/random', (req, res) => {
-  res.send('get testimonials/random');
+  const dbLength = db.length;
+
+  if (dbLength === 0) {
+    return res.json({ data: 'no elements in database' });
+  }
+
+  const testimonial = db[Math.floor(Math.random() * dbLength)];
+
+  res.json({ data: testimonial });
+});
+
+app.get('/testimonials/:id', (req, res) => {
+  const { id } = req.params;
+  const testimonial = db.find((item) => item.id == id);
+
+  if (testimonial) {
+    return res.json({ data: testimonial });
+  } else {
+    return res.json({ data: 'not found' });
+  }
 });
 
 app.post('/testimonials', (req, res) => {
-  res.send('post testimonials');
+  if (!req.body) {
+    return res.json({ message: 'missing all data' });
+  }
+
+  if (!req.body.author || !req.body.text) {
+    return res.json({ message: 'missing data' });
+  }
+
+  const { author, text } = req.body;
+
+  db.push({ id: uuidv4(), author, text });
+
+  res.json({ message: 'OK' });
 });
 
 app.put('/testimonials/:id', (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    return res.json({ message: 'missing data to update' });
+  }
+
+  const { id } = req.params;
+  // finished on PUT Request
+  // https://kodilla.com/pl/bootcamp-module/1582/276/9102#submodule-1120
+
+  console.log(id);
+  console.log(req.query);
+
   res.send('put testimonials/id');
 });
 

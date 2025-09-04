2a1beea12de1f248148df87d9debbd95dca19b5a
Dariusz Okoński <darek200180@gmail.com>
2025-09-04 08:16:01 +0200

    add endpoints for seats

--- routes/seats.js
+++ routes/seats.js
@@ -7,4 +7,86 @@ router.get('/', (req, res) => {
   res.json({ data: db.seats });
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const seats = db.seats.find((item) => item.id == id);
+
+  if (seats) {
+    return res.json({ data: seats });
+  } else {
+    return res.status(404).json({ message: 'not found' });
+  }
+});
+
+router.post('/', (req, res) => {
+  if (!req.body) {
+    return res.status(400).json({ message: 'missing all data' });
+  }
+
+  if (!req.body.day || !req.body.seat || !req.body.client || !req.body.email) {
+    return res.status(400).json({ message: 'missing some data' });
+  }
+
+  const { day, seat, client, email } = req.body;
+
+  db.seats.push({ id: uuidv4(), day, seat, client, email });
+
+  res.json({ message: 'OK' });
+});
+
+router.put('/:id', (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    return res.status(400).json({ message: 'missing data to update' });
+  }
+
+  const { id } = req.params;
+
+  const seat = db.seats.find((item) => item.id === id);
+
+  if (!seat) {
+    return res
+      .status(404)
+      .json({ message: 'this element does not exists in database' });
+  }
+
+  if (req.query.day) {
+    seat.day = Number(req.query.day);
+  }
+
+  if (req.query.seat) {
+    seat.seat = Number(req.query.seat);
+  }
+  if (req.query.client) {
+    seat.client = req.query.client;
+  }
+  if (req.query.email) {
+    seat.email = req.query.email;
+  }
+
+  db.seats = db.seats.map((item) => {
+    if (item.id === id) {
+      return seat;
+    }
+    return item;
+  });
+
+  res.json({ message: 'OK' });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const seats = db.seats.find((item) => item.id === id);
+
+  if (!seats) {
+    return res
+      .status(404)
+      .json({ message: 'this element does not exists in database' });
+  }
+
+  db.seats = db.seats.filter((item) => item.id !== id);
+
+  res.json({ message: 'OK' });
+});
+
 module.exports = router;

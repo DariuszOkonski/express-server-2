a9a77ce3ef95c20e8965aa2843383e08a94ddc57
Dariusz Okoński <darek200180@gmail.com>
2025-09-04 08:32:53 +0200

    install cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const testimonialsRoutes = require('./routes/testimonials');
 const concertsRoutes = require('./routes/concerts');
@@ -7,7 +8,7 @@ const seatsRoutes = require('./routes/seats');
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cors());
+app.use(cors());
 
 // Routes
 app.use('/api/testimonials', testimonialsRoutes);
@@ -19,7 +20,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

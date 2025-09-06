1f53200cb66b395495b7d0eb09e9a1de2a530d39
Dariusz Okoński <darek200180@gmail.com>
2025-09-06 12:04:49 +0200

    remove settimeouts

--- client/src/redux/seatsRedux.js
+++ client/src/redux/seatsRedux.js
@@ -33,7 +33,6 @@ export const loadSeatsRequest = () => {
     try {
       let res = await axios.get(`${API_URL}/seats`);
 
-      await new Promise((resolve) => setTimeout(resolve, 2000));
       dispatch(loadSeats(res.data.data));
       dispatch(endRequest({ name: 'LOAD_SEATS' }));
     } catch (e) {
@@ -47,7 +46,6 @@ export const addSeatRequest = (seat) => {
     dispatch(startRequest({ name: 'ADD_SEAT' }));
     try {
       let res = await axios.post(`${API_URL}/seats`, seat);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
       dispatch(addSeat(res));
       dispatch(endRequest({ name: 'ADD_SEAT' }));
     } catch (e) {

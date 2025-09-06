d04f16a118779b1a95ec6a5452ef98ea10215f2c
Dariusz Okoński <darek200180@gmail.com>
2025-09-06 11:38:30 +0200

    remove unused console logs

--- client/src/redux/seatsRedux.js
+++ client/src/redux/seatsRedux.js
@@ -28,7 +28,6 @@ export const addSeat = (payload) => ({ payload, type: ADD_SEAT });
 /* THUNKS */
 
 export const loadSeatsRequest = () => {
-  console.log('loadSeatsRequest');
   return async (dispatch) => {
     dispatch(startRequest({ name: 'LOAD_SEATS' }));
     try {
